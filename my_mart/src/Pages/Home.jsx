c4533dc459f95fcba90113fb3eff9d48721b585c
sartaza123 <sartaza123@gmail.com>
2026-02-07 03:32:22 +0530

    FetchApi using custom hook useFetch

--- my_mart/src/Pages/Home.jsx
+++ my_mart/src/Pages/Home.jsx
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 import ProductList from "../components/ProductList";
-=======
-import { useEffect, useState } from "react";
-import { getAllProducts } from "../utils/productApi";
->>>>>>> 0389a4f2b9fcb9670b00111cc1ebeed5d4552169
-
 function Home() {
-  const [product, setProduct] = useState([]);
-  useEffect(() => {
-    getAllProducts().then(setProduct).catch(console.error);
-  }, []);
-  console.log(product);
   return (
     <>
       <ProductList />
